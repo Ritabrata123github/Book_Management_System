6df44cb9b9ee71ba1ae2d7373d315f8e2bdc01f4
Ritabrata123github <121775173+Ritabrata123github@users.noreply.github.com>
2026-03-06 18:02:51 +0530

    Update BookDAOImpl.java

--- BookDAOImpl.java
+++ BookDAOImpl.java
@@ -1,5 +1,97 @@
 package com;
 
-public class BookDAOImpl {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class BookDAOImpl implements BookDAO {
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("book-unit");
+
+    public void addBook(Book b) {
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.persist(b);
+
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    public void viewAllBooks() {
+
+        EntityManager em = emf.createEntityManager();
+
+        Query q = em.createQuery("select b from Book b");
+
+        List<Book> list = q.getResultList();
+
+        for(Book b : list)
+        {
+            System.out.println(b.getId()+" "+b.getTitle()+" "+b.getAuthor()+" "+b.getPrice());
+        }
+
+        em.close();
+    }
+
+    public void updatePrice(int id,String author,double price) {
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Query q = em.createQuery("update Book b set b.price=:p where b.id=:i and b.author=:a");
+
+        q.setParameter("p", price);
+        q.setParameter("i", id);
+        q.setParameter("a", author);
+
+        q.executeUpdate();
+
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    public void deleteBook(int id) {
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Book b = em.find(Book.class, id);
+
+        if(b!=null)
+        {
+            em.remove(b);
+        }
+
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    public void displayByTitle(String title) {
+
+        EntityManager em = emf.createEntityManager();
+
+        Query q = em.createQuery("select b from Book b where b.title=:t");
+
+        q.setParameter("t", title);
+
+        List<Book> list = q.getResultList();
+
+        for(Book b : list)
+        {
+            System.out.println(b.getId()+" "+b.getTitle()+" "+b.getAuthor()+" "+b.getPrice());
+        }
+
+        em.close();
+    }
 }
