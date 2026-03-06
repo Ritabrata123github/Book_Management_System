b0f3a84d8f79cca8f48c06f88cbfd9846d78bd89
Ritabrata123github <121775173+Ritabrata123github@users.noreply.github.com>
2026-03-06 18:03:28 +0530

    Update BookDAO.java

--- BookDAO.java
+++ BookDAO.java
@@ -1,5 +1,15 @@
 package com;
 
-public class BookDAO {
+public interface BookDAO {
+
+    void addBook(Book b);
+
+    void viewAllBooks();
+
+    void updatePrice(int id,String author,double price);
+
+    void deleteBook(int id);
+
+    void displayByTitle(String title);
 
 }
